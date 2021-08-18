f8c4ffc215df6e06aa3492320d6746ac2ebaa27a
YWTechIT <abcd8637@naver.com>
2021-08-18 22:08:37 +0900

    ADD data types

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface Data {
+    category_id: number;
+    contents: string;
+    created_at: string;
+    id: number;
+    title: string;
+    updated_at: string;
+    user_id: number;
+}
