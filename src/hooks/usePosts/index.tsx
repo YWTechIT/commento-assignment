1201403ada6076f00add8b9555bfa48dbd9d21fb
YWTechIT <abcd8637@naver.com>
2021-09-06 17:08:47 +0900

    Change type of getPosts parameter
    getPosts의 변수 type 변경 및 네이밍

--- src/hooks/usePosts/index.tsx
+++ src/hooks/usePosts/index.tsx
@@ -2,31 +2,30 @@ import axios from "axios";
 import { useInfiniteQuery } from "react-query";
 import { Data, SortType } from "../../types";
 
-type QueryKeyType = Array<string | QueryParam>
-type QueryParam = {sort: string}
+type QueryKeyType = Array<string | SortType>
 type PostResponse = {data: Data[], pageParam: number}
 
 async function getPosts(queryKey: QueryKeyType, pageParam: number): Promise<PostResponse> {
-  let {sort} = queryKey[1] as QueryParam;
+  const sortParam = queryKey[1] as SortType;
 
-  const listResponse = await axios.get("https://problem.comento.kr/api/list", {
+  const response = await axios.get("https://problem.comento.kr/api/list", {
     params: {
       headers: "Accept: application/json",
       page: pageParam,
-      ord: sort,
+      ord: sortParam,
       category: [1, 2, 3],
       limit: 10,
     },
   });
 
   return {
-    data: listResponse.data.data,
+    data: response.data.data,
     pageParam: pageParam + 1,
   };
 }
 
-export function usePosts(sortFilter: SortType) {
-  return useInfiniteQuery(["usePosts", sortFilter], ({queryKey, pageParam=1}) => getPosts(queryKey, pageParam), {
+export function usePosts(postParams: SortType) {
+  return useInfiniteQuery(["usePosts", postParams], ({queryKey, pageParam=1}) => getPosts(queryKey, pageParam), {
     getNextPageParam: (nextPage) => nextPage.pageParam ?? false,
   });
 }
