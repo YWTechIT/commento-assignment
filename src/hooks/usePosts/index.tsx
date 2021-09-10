49ff4a073f60caf3764cd4ef9f6000832f9159f8
YWTechIT <abcd8637@naver.com>
2021-09-10 14:38:08 +0900

    Change return data value
    scrap 기능 추가를 위해 `data`변수에 `boolean` 형태인 `complete`  프로퍼티를

--- src/hooks/usePosts/index.tsx
+++ src/hooks/usePosts/index.tsx
@@ -2,30 +2,40 @@ import axios from "axios";
 import { useInfiniteQuery } from "react-query";
 import { Data, SortType } from "../../types";
 
-type QueryKeyType = Array<string | SortType>
-type PostResponse = {data: Data[], pageParam: number}
+type QueryKeyType = Array<string | SortType>;
+type PostResponse = { data: Data[]; pageParam: number };
 
-async function getPosts(queryKey: QueryKeyType, pageParam: number): Promise<PostResponse> {
-  const sortParam = queryKey[1] as SortType;
+async function getPosts(
+    queryKey: QueryKeyType,
+    pageParam: number
+): Promise<PostResponse> {
+    const sortParam = queryKey[1] as SortType;
 
-  const response = await axios.get("https://problem.comento.kr/api/list", {
-    params: {
-      headers: "Accept: application/json",
-      page: pageParam,
-      ord: sortParam,
-      category: [1, 2, 3],
-      limit: 10,
-    },
-  });
+    const response = await axios.get("https://problem.comento.kr/api/list", {
+        params: {
+            headers: "Accept: application/json",
+            page: pageParam,
+            ord: sortParam,
+            category: [1, 2, 3],
+            limit: 10,
+        },
+    });
 
-  return {
-    data: response.data.data,
-    pageParam: pageParam + 1,
-  };
+    return {
+        data: response.data.data.map(function (item: Data) {
+            item["complete"] = false;
+            return item;
+        }),
+        pageParam: pageParam + 1,
+    };
 }
 
 export function usePosts(postParams: SortType) {
-  return useInfiniteQuery(["usePosts", postParams], ({queryKey, pageParam=1}) => getPosts(queryKey, pageParam), {
-    getNextPageParam: (nextPage) => nextPage.pageParam ?? false,
-  });
+    return useInfiniteQuery(
+        ["usePosts", postParams],
+        ({ queryKey, pageParam = 1 }) => getPosts(queryKey, pageParam),
+        {
+            getNextPageParam: (nextPage) => nextPage.pageParam ?? false,
+        }
+    );
 }
